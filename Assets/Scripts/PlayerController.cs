5c1b058e518af7926bb42cf5e70be179b114e362
Sheraz190 <sherazi0190@gmail.com>
2025-09-17 16:02:27 +0500

    Adding Attack Buttons

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
     public static PlayerController Instance;
 
     [SerializeField] private GameObject player;
-    [SerializeField] private Rigidbody2D _playerRb;
+    [SerializeField] private Rigidbody2D playerRb;
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject canvas;
 
@@ -19,12 +19,8 @@ public class PlayerController : MonoBehaviour
 
     private bool _canMoveLeft;
     private bool _canMoveRight;
-
-    private float _duration = 3.5f;
-    private float _elapsedTime = 0.0f;
-    private float _currentSpeed = 0;
     private float _maxSpeed = 20;
-   
+    private int _jumpForce = 20;
     #endregion
 
     private void Awake()
@@ -33,7 +29,7 @@ public class PlayerController : MonoBehaviour
         {
             Instance = this;
         }
-        _playerRb = GetComponent<Rigidbody2D>();
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     private void Start()
@@ -48,7 +44,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        CheckKeyboardInputs();
+       // CheckKeyboardInputs();
         if (_canMoveLeft || _canMoveRight)
         {
             Movings();
@@ -78,63 +74,19 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void CheckInputs()
-    {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            IncreaseSpeed();
-            SetWalkTrue();
-            _playerRb.linearVelocity = new Vector2(-_currentSpeed, _playerRb.linearVelocity.y);
-            transform.localScale = new Vector2(-_originalScale.x, _originalScale.y);
-        }
-       
-       else if (Input.GetKey(KeyCode.RightArrow))
-       {
-            IncreaseSpeed();
-            SetWalkTrue();
-            _playerRb.linearVelocity = new Vector2(_currentSpeed, _playerRb.linearVelocity.y);
-            transform.localScale = new Vector2(_originalScale.x, _originalScale.y);
-       }
-
-        else
-        {
-            _elapsedTime = 0;
-            _currentSpeed = 0;
-            _playerRb.linearVelocity = new Vector2(0, _playerRb.linearVelocity.y);
-            BackToIdleState();
-        }
-    }
-
-    private void IncreaseSpeed()
-    {
-        if (_elapsedTime < _duration)
-        {
-            _elapsedTime += Time.deltaTime;
-            float t = _elapsedTime / _duration;
-            _currentSpeed = Mathf.Lerp(0, _maxSpeed, t);
-        }
-    }
-
     private void Movings()
     {
         if (_canMoveRight)
         {
-            IncreaseSpeed();
             SetWalkTrue();
-            _playerRb.linearVelocity = new Vector2(_currentSpeed, _playerRb.linearVelocity.y);
+            playerRb.linearVelocity = new Vector2(_maxSpeed, playerRb.linearVelocity.y);
             transform.localScale = new Vector2(_originalScale.x, _originalScale.y);
         }
         else if (_canMoveLeft)
         {
-            IncreaseSpeed();
             SetWalkTrue();
-            _playerRb.linearVelocity = new Vector2(-_currentSpeed, _playerRb.linearVelocity.y);
+            playerRb.linearVelocity = new Vector2(-_maxSpeed, playerRb.linearVelocity.y);
             transform.localScale = new Vector2(-_originalScale.x, _originalScale.y);
-        }
-        else
-        {
-            _elapsedTime = 0;
-            _currentSpeed = 0;
         }
     }
 
@@ -154,8 +106,34 @@ public class PlayerController : MonoBehaviour
         _canMoveLeft = false;
         if (!_canMoveRight && !_canMoveLeft)
         {
-            _playerRb.linearVelocity = new Vector2(0, _playerRb.linearVelocity.y);
+            playerRb.linearVelocity = new Vector2(0, playerRb.linearVelocity.y);
             BackToIdleState();
+        }
+    }
+
+    public void Jump()
+    {
+        if (_isGrounded)
+        {
+            SetJumpAnimation();
+            playerRb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            StartCoroutine(DropDown());
+            _isGrounded = false;
+        }
+    }
+
+    public IEnumerator DropDown()
+    {
+        yield return new WaitForSeconds(0.5f);
+        playerRb.linearVelocity = new Vector2(playerRb.linearVelocity.x, -10f);
+        StopJumpAnim();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
         }
     }
 
@@ -172,23 +150,6 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("Running", false);
     }
 
-    public void Jump()
-    {
-        if (_isGrounded )
-        {
-            _playerRb.AddForce(Vector2.up * 18, ForceMode2D.Impulse);
-            StartCoroutine(DropDown());
-            _isGrounded = false;
-            SetJumpAnimation();
-        }
-    }
-
-    public IEnumerator DropDown()
-    {
-        yield return new WaitForSeconds(0.5f);
-        _playerRb.linearVelocity = new Vector2(_playerRb.linearVelocity.x, -10f);
-    }
-
     private void SetJumpAnimation()
     {
         animator.SetBool("Jumping", true);
@@ -196,30 +157,14 @@ public class PlayerController : MonoBehaviour
 
     private void StopJumpAnim()
     {
-        animator.SetBool("Jumping", false); 
+        animator.SetBool("Jumping", false);
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.CompareTag("Ground"))
-        {
-            _isGrounded = true;
-            StopJumpAnim();
-        }
-    }
-
-    private void MoveDownToGround()
-    {
-        Debug.Log("Method move down Called");
-        StartCoroutine(DropDown());
-        StopMoving();
-    }
-
     public void SetAttackAnimation()
     {
         animator.SetBool("Attack", true);
         StartCoroutine(StopAttackAnimation());
     }
-        
+
     private IEnumerator StopAttackAnimation()
     {
         yield return new WaitForSeconds(0.55f);
@@ -237,6 +182,7 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(0.75f);
         animator.SetBool("Vanish", false);
     }
+
     public void SetDeathAnimation()
     {
         animator.SetBool("Death", true);
